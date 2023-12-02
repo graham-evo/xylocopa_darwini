b69b23f809faa1ed223e592aba1daccbe905b4a3
Graham M <122396679+graham-evo@users.noreply.github.com>
2023-12-01 20:42:26 -0500

    Update busco.sh

--- reference_genome/xd.sc.m_assembly/shell_scripts/busco.sh
+++ reference_genome/xd.sc.m_assembly/shell_scripts/busco.sh
@@ -6,4 +6,4 @@ busco -i xd.sc.m.contigs.fasta \ # name of input assembly fasta file
     -l hymenopetera_odb10 \ # lineage database to compare single copy orthologs
     -m genome \ # method, 'GENOME' for reference genoems
     -o busco_xd.sc.m \ # output file directory
-    -c 12 # 12 cpu's
+    -c 12 # 12 cpus

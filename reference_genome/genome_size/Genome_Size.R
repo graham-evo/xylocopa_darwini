e972c0f0908989f46f01a5c26aab0801326bc560
graham-evo <grahamcm@email.sc.edu>
2024-01-05 16:16:39 -0500

    Genome Size and Hifi Read R scripts polished for use with Quarto

--- reference_genome/genome_size/Genome_Size.R
+++ reference_genome/genome_size/Genome_Size.R
@@ -1,44 +1,104 @@
 # Author: Graham McLaughlin
 # Date Created: 12-02-2023
-# Date Modified: 12-02-2023
+# Date Modified: 01-05-2023
 
-# Estimating Genome Size Using Raw Read-to-Assembly Mapping Approach
+# Estimating Genome Size Using A variety of Approaches
 # References:
 #- <https://doi.org/10.1111/1755-0998.13570>
 #- <https://doi.org/10.1093/gbe/evx032>
 #- <https://github.com/schellt/backmap/blob/master/README.md>
 
-library(fitdistrplus)
-library(truncdist)
-library(splitstackshape)
+## ---- Coverage ----
 library(tidyverse)
 
-cov.freq <- read.table("cov.txt")
+cov.freq <- read.table(here::here("reference_genome/assembly_comparisons/coverage/cov.txt"), header = TRUE)
 colnames(cov.freq) <- c("contig", "freq", "coverage")
 
 head(cov.freq)
 tail(cov.freq)
 summary(cov.freq$coverage)
 
-plot(cov.freq$coverage, cov.freq$freq, type = "l")
-
 ggplot(cov.freq, aes(x=coverage)) +
   geom_histogram(binwidth = 0.5, alpha=0.5,
                  position="dodge",
                  color="gold3") +
+  labs(x = "Coverage", y = "Number of Bases") +
   theme_bw()
 
+## ---- basic-method ----
+avg_cov = 53.47
+avg_length = 10640.6
+num_reads = 1071017
+gen_size = (avg_length*num_reads)/avg_cov
+gen_size
+
+## ---- 19mer-counting-method ----
+## Estimation of Genome Characteristics Using K-mer Count Frequencies from Jellyfish-2.1.0
+# Starting with a k-mer of length 19
+
+k19mer_counts <- read.table(here::here("reference_genome/genome_size/k-mer_counts/19mer_counts.histo"))
+colnames(k19mer_counts) <- c("k19","freq")
+
+summary(k19mer_counts)
+#barplot(k19mer_counts$freq, xlim = c(1,100),ylim = c(0,5000000))
+plot(k19mer_counts[5:200,], type="l",
+     xlab = "K-mers of length 19",
+     ylab = "Frequency")
+# Estimating genome size
+error = 14
+peak = 57
+g_est <- round(sum(as.numeric(k19mer_counts[error:nrow(k19mer_counts),1]*k19mer_counts[error:nrow(k19mer_counts),2]))/peak, digits = 0)
+g_est
+
+# Estimating Coverage
+total_bp = 11500000000 # 11.5 Gigabases
+cov <- round(total_bp/g_est, digits = 2)
+cov
+
+## ---- 31mer-counting-method ----
+k31mer_counts <- read.table("31mer_counts.histo")
+colnames(k31mer_counts) <- c("k31","freq")
+
+summary(k31mer_counts)
+barplot(k31mer_counts$freq, xlim = c(1,100),ylim = c(0,5000000))
+
+# Estimating genome size
+error = 14
+peak = 57
+g_est <- round(sum(as.numeric(k31mer_counts[error:nrow(k31mer_counts),1]*k31mer_counts[error:nrow(k31mer_counts),2]))/peak, digits = 0)
+g_est # 298.79 Megabases or 298 million base pairs
+
+# Estimating Coverage
+total_bp = 11500000000 # 11.5 Gigabases
+cov <- round(total_bp/g_est, digits = 2)
+cov
+
+## ---- backmapping-method ----
+error = 16
+peak = 58
+g_est <- round(sum(as.numeric(kmer_counts[error:nrow(kmer_counts),1]*kmer_counts[error:nrow(kmer_counts),2]))/peak, digits = 0)
+g_est # 298.79 Megabases or 298 million base pairs
+
+# Estimating Coverage
+total_bp = 11500000000 # 11.5 Gigabases
+cov <- round(total_bp/g_est, digits = 2)
+cov
+
+library(fitdistrplus)
+library(truncdist)
+library(splitstackshape)
+
+cov.freq <- expandRows(cov.freq$freq)
+obj <- as.vector(cov.freq$freq)
 #transform Qualimap output to R-object
 #define function for mode
-mode <- function(cov.freq) {uniqv <- unique(cov.freq) uniqv[which.max(tabulate(match(cov.freq, uniqv)))]}
-min <- mode – 5
-max <- mode + 5
+mode <- function(obj) {uniqv <- unique(obj)
+  uniqv[which.max(tabulate(match(obj, uniqv)))]}
+uniqv <- unique(obj)
+mode <- uniqv[which.max(tabulate(match(obj, uniqv)))]
+min <- 2 - 5
+max <- 2 + 5
 dtruncated_poisson <- function(x, lambda) {dtrunc(x, "pois", a=min, b=max, lambda=lambda)}
 ptruncated_poisson <- function(q, lambda) {ptrunc(q, "pois", a=min, b=max, lambda=lambda)}
-fitdist(obj, "pois", start = list(lambda = mode))
-
-
-  
-plot(x[,1],x[,2],
-log=\"x\",type=\"l\",xlab=\"Coverage\",
-ylab=\"Count\",main=\"$assembly\\n$tech\")\n
+fitdist(obj, "pois", start = c(lambda = mode))
+fitdist(obj, "truncated_poisson", start = c(-3,7))
--- reference_genome/raw_reads/HifiReads_eval.R
+++ reference_genome/raw_reads/HifiReads_eval.R
@@ -1,47 +1,46 @@
 # Author: Graham C. McLaughlin
 # Date Created: November 30th 2023
-# Date Modified: November 30th 2023
+# Date Modified: January 5th 2023
 
 library(ggplot2)
 library(dplyr)
 library(cowplot) 
-library(egg)
+library(egg) 
 
-## Estimating read length estimates from Arizona Genomics Insitute Hifi Reads after processing
-##with ccs software and parsing to a .csv file with bioawk
+## Estimating read length estimates from Arizona Genomics Insitute Hifi Reads after preprocessing
+#with ccs software and parsing to a .csv file with bioawk
 
-read_length_df <- read.csv("KimKim_length.csv") # Read in bioawk parsed read lengths
+read_length_df <- read.csv(here::here("reference_genome/raw_reads/KimKim_length.csv")) # Read in bioawk parsed read lengths
 colnames(read_length_df) <- c("PacBio_HiFi","length") # Change column names
 head(read_length_df) # check for correction
+dim(read_length_df)
 
-# Calculate the average read-length:
-
-# Using Dplyr
+## ---- read-stats ----
 summary_df <- read_length_df %>%
-  summarise(grp.mean=mean(length),
-            grp.median=median(length),
-            grp.max=max(length))
+  summarise(Mean.Length= mean(length),
+            Median.Length= median(length),
+            Max.Length= max(length),
+            Num.Reads = length(read_length_df$length),
+            Num.Bases = sum(length))
+summary_df
 
-# or base R
-mean(read_length_df$length)
-
+## ---- Plotting ----
 # Draw a read-length distribution plot for all reads:
 total.length.plot <- ggplot(read_length_df,
                             aes(x=length)) +
   geom_histogram(binwidth=100,
-                 alpha=0.5,
                  position="dodge",
-                 color="gold3") +
+                 fill="gold3") +
   geom_vline(data=summary_df,
-             aes(xintercept=grp.mean),
+             aes(xintercept=Mean.Length),
              linetype="solid",
              size =0.4) +
   geom_vline(data=summary_df,
-             aes(xintercept=grp.median),
+             aes(xintercept=Median.Length),
              linetype="dashed",
              size =0.4) +
   geom_vline(data=summary_df,
-             aes(xintercept=grp.max),
+             aes(xintercept=Max.Length),
              linetype="dashed",
              size =0.4) +
   scale_x_continuous(limits = c(0, max(read_length_df$length)),
@@ -55,19 +54,18 @@ total.length.plot <- ggplot(read_length_df,
 zoomed.length.plot <- ggplot(read_length_df,
                             aes(x=length)) +
   geom_histogram(binwidth=100,
-                 alpha=0.5,
                  position="dodge",
-                 color="gold3") +
+                 fill="gold3") +
   geom_vline(data=summary_df,
-             aes(xintercept=grp.mean),
+             aes(xintercept=Mean.Length),
              linetype="solid",
              size =0.4) +
   geom_vline(data=summary_df,
-             aes(xintercept=grp.median),
+             aes(xintercept=Median.Length),
              linetype="dashed",
              size =0.4) +
   geom_vline(data=summary_df,
-             aes(xintercept=grp.max),
+             aes(xintercept=Max.Length),
              linetype="dashed",
              size =0.4) +
   scale_x_continuous(limits = c(0, 20000),
@@ -78,34 +76,42 @@ zoomed.length.plot <- ggplot(read_length_df,
        y= "Frequency of Reads") +
   theme_bw()
   
-#kb.length.plot <- ggplot(read_length_df,
-                              aes(x=length)) +
-    geom_histogram(binwidth=50,
-                   alpha=0.5,
-                   position="dodge",
-                   color = "gold3") +
-    geom_vline(data=summary_df,
-               aes(xintercept=grp.mean),
-               linetype="solid",
-               size =0.4) +
-    geom_vline(data=summary_df,
-               aes(xintercept=grp.median),
-               linetype="dashed",
-               size =0.4) +
-    geom_vline(data=summary_df,
-               aes(xintercept=grp.max),
-               linetype="dashed",
-               size =0.4) +
-    scale_x_continuous(limits = c(0, 20000),
-                       breaks = seq(0, max(read_length_df$length),2500)) +
-    scale_y_continuous(limits = c(0, 13000),
-                       breaks = seq(0, 13000, 2000)) +
-    labs(x="Read Length (bp)",
-         y= "Frequency of Reads") +
-    theme_bw() 
-  
 # Merge both the read-length distribution plots 
+## ---- read_length_plot ----
 plot <- plot_grid(total.length.plot, zoomed.length.plot, ncol = 1) # Save the figure using the file name, ‘‘read.length.pdf’’ pdf("read.length.pdf",width=6,height=8,paper=’special’) print(plot)
 ggarrange(total.length.plot,
           zoomed.length.plot
             )
+
+## ---- read-quality-metrics ----
+# Sequencing metrics:
+hifi_reads <- read.csv(here::here("reference_genome/raw_reads/zmw_metrics.csv"))
+head(hifi_reads) 
+dim(hifi_reads) # 1,802,366 total reads
+hifi_reads <- subset(hifi_reads, read_qual >= 0.99 & num_passes >= 3)
+dim(hifi_reads) # 1,085,872 reads that meet the quality threshold
+
+mean(hifi_reads$read_length) # 10,640 mean read length
+sd(hifi_reads$read_length) # +/- 5299.36 bps
+max(hifi_reads$read_length) # max read length is 72,979 bp
+min(hifi_reads$read_length) # min read length is 105 bp
+dim(hifi_reads)
+
+summary(hifi_reads$read_length) # confirming above calculations
+
+summary(hifi_reads$read_qual)
+
+ggplot(hifi_reads, aes(x=read_qual)) +
+  geom_histogram(position="dodge",
+                 fill="gold3") +
+  labs(x = "Quality Score (%)", y = "Number of Reads") +
+  theme_bw()
+
+ggplot(hifi_reads, aes(x=num_passes)) +
+  geom_histogram(position="dodge",
+                 fill="gold3") +
+  labs(x = "Number of Passes", y = "Number of Reads") +
+  theme_bw()
+
+
+
